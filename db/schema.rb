93c8bcd75349ddb332bdcbdbb63826aab2ac1cfb
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-10 15:04:08 +0900

    remove index from name column of chat_groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109100204) do
+ActiveRecord::Schema.define(version: 20170110055819) do
 
   create_table "chat_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_chat_groups_on_name", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
