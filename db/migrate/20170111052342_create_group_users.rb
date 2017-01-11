d3f636fff5d561e421d442ba1e70584a63f060ac
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-11 14:38:32 +0900

    modify migration file

--- db/migrate/20170111052342_create_group_users.rb
+++ db/migrate/20170111052342_create_group_users.rb
@@ -1,8 +1,8 @@
 class CreateGroupUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :group_users do |t|
-      t.integer :group_id, null: false, t.reference :chat_groups
-      t.integer :user_id, null: false, t.reference :user
+      t.references :chat_groups, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110055819) do
+ActiveRecord::Schema.define(version: 20170111052342) do
 
   create_table "chat_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "chat_groups_id"
+    t.integer  "user_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["chat_groups_id"], name: "index_group_users_on_chat_groups_id", using: :btree
+    t.index ["user_id"], name: "index_group_users_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -37,4 +46,6 @@ ActiveRecord::Schema.define(version: 20170110055819) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "group_users", "chat_groups", column: "chat_groups_id"
+  add_foreign_key "group_users", "users"
 end
