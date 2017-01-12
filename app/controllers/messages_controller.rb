f8eaaac438e9b4520e63bafb161c4b44a8c28fd7
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-12 20:12:18 +0900

    show all chat_group which current_user joins

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,5 +2,7 @@ class MessagesController < ApplicationController
 
   def index
     @chat_group = ChatGroup.find(params[:chat_group_id])
+    group_user_ids = current_user.chat_group_ids
+    @chat_groups = group_user_ids.map{ |id| ChatGroup.find(id) }
   end
 end
