a756c04ce33ba9d91dc0e6a40650b3b7ecb751dc
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-12 11:15:11 +0900

    add messages action for chat_group

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'chat_groups#index'
-  resources :chat_groups, except: [:show, :destroy]
+  resources :chat_groups, except: [:show, :destroy] do
+    member do
+      get 'messages'
+    end
+  end
 end
