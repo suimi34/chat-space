048e450475be72a758e74ec0dc664edd74723dc5
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-13 18:27:07 +0900

    define @message in message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,5 +4,6 @@ class MessagesController < ApplicationController
     @chat_group = ChatGroup.find(params[:chat_group_id])
     group_user_ids = current_user.chat_group_ids
     @chat_groups = group_user_ids.map{ |id| ChatGroup.find(id) }
+    @message = Message.new
   end
 end
