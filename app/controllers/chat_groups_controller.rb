9dd8f6baaada60cdb2c6655f481311e2f3ba4c2b
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-13 16:08:03 +0900

    add flash message after creating new group

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -12,7 +12,7 @@ class ChatGroupsController < ApplicationController
   def create
     @chat_group = ChatGroup.new(chat_group_params)
     if @chat_group.save
-      redirect_to chat_group_messages_path(@chat_group)
+      redirect_to chat_group_messages_path(@chat_group), notice: 'チャットグループが作成されました。'
     else
       render :new
     end
