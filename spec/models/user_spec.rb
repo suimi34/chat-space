cf02a9803275249bc7f7da4fb76f68a9097d4856
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-16 19:55:42 +0900

    write test code for user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe User do
+  describe '#create' do
+
+    it "is invalid without a name" do
+      user = build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+  end
+
+  it "is invalid without an email" do
+    user = build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+end
