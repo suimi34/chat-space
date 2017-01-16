9f163e76d449f62b7fc472fbe78dc0ac58852e41
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-16 20:06:49 +0900

    change error message to Japanese

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,13 +6,13 @@ describe User do
     it "is invalid without a name" do
       user = build(:user, name: nil)
       user.valid?
-      expect(user.errors[:name]).to include("can't be blank")
+      expect(user.errors[:name]).to include("ユーザー名は空ではいけません")
     end
   end
 
   it "is invalid without an email" do
     user = build(:user, email: nil)
     user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
+    expect(user.errors[:email]).to include("メールアドレスが空です")
   end
 end
