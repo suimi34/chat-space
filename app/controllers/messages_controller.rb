c9094620b60c3ad463000aebca060f7eacb1f60b
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-16 14:41:29 +0900

    add create action and strong parameter

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,4 +6,18 @@ class MessagesController < ApplicationController
     @users = @chat_group.users
     @message = Message.new
   end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to chat_group_messages_path(@chat_group)
+    else
+      render :index
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(body: params[:body], chat_group_id: params[:chat_group_id], user_id: params[:user_id])
+  end
 end
