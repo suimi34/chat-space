472d73a5d9e28c40c5bec6c4f6d20f92c2191f66
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-16 14:40:08 +0900

    add routing for create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'chat_groups#index'
   resources :chat_groups, except: [:show, :destroy] do
-    resources :messages, only: :index
+    resources :messages, only: [:index, :create]
   end
 end
