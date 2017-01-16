17bd9ae19f30a25281b7d3c7680da9fec0c00c8a
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-16 15:51:55 +0900

    modify create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,23 @@
 class MessagesController < ApplicationController
 
   def index
-    @chat_group = ChatGroup.find(params[:chat_group_id])
+    set_chat_group_info
     @chat_groups = current_user.chat_groups
-    @users = @chat_group.users
     @message = Message.new
   end
 
   def create
-    @message = Message.new(message_params)
-    if @message.save
-      redirect_to chat_group_messages_path(@chat_group)
-    else
-      render :index
-    end
+    set_chat_group_info
+    @message = Message.create(body: message_params[:body], chat_group_id: @chat_group.id, user_id: current_user.id)
   end
 
   private
   def message_params
-    params.require(:message).permit(body: params[:body], chat_group_id: params[:chat_group_id], user_id: params[:user_id])
+    params.require(:message).permit(:body)
+  end
+
+  def set_chat_group_info
+    @chat_group = ChatGroup.find(params[:chat_group_id])
+    @users = @chat_group.users
   end
 end
