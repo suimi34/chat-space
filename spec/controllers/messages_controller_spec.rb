49bbd9a4e4e9a572f4338f001790d9965bb0262a
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-17 18:31:21 +0900

    add test code for messages index

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 require 'faker'
+require 'messages_controller'
 
 describe MessagesController do
+  describe "GET #index" do
+    it "populates an array of messages" do
+      messages = create_list(:message, 3)
+      get :index, params: { id: 1 }, use_routes: :chat_group_messages
+      expect(assigns(:messages)).to match_array(messages)
+    end
 
+    it "renders the :index template" do
+      get :index, params: { id: 1 }
+      expect(response).to render_template :index
+    end
+  end
 end
