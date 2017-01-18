54fa5ec977d6908e67d157c23bb9c578008ae9e7
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-18 16:49:20 +0900

    modify controll marcos file

--- spec/support/controller_marcos.rb
+++ spec/support/controller_marcos.rb
@@ -1,10 +1,5 @@
 module ControllerMacros
-  def login_admin(admin)
-    @request.env["devise.mapping"] = Devise.mappings[:admin]
-    sign_in admin
-  end
-
-  def login_user(user)
+  def login_user
     @request.env["devise.mapping"] = Devise.mappings[:user]
     user.confirm!
     sign_in user
