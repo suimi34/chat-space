f6e018647d42e268f4afd1c171a1e1a0f2e7740a
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-19 11:50:43 +0900

    modify test code for populates an array of messages

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -6,22 +6,19 @@ describe MessagesController, type: :controller do
   describe "GET #index" do
     let(:user) { FactoryGirl.build(:user) }
     before do
-      @user = create(:user)
-      sign_in @user
+      user = create(:user)
+      sign_in user
     end
 
     it "populates an array of messages" do
-      messages = create_list(:message, 5, chat_group_id: 1)
-      get :index, params: { id: 1 }
+      chat_group = create(:chat_group)
+      messages = create_list(:message, 5, chat_group_id: chat_group.id)
+      get :index, params: { chat_group_id: chat_group }, use_routes: chat_group_messages_path(chat_group)
       expect(assigns(:messages)).to match_array(messages)
     end
 
     it "renders the :index template" do
       chat_group = create(:chat_group)
-      chat_groups = create_list(:chat_group, 5)
-      message = create(:message, chat_group_id: chat_group.id)
-      messages = create_list(:message, 5, chat_group_id: chat_group.id)
-      binding.pry
       get :index, params: { chat_group_id: chat_group }, use_routes: chat_group_messages_path(chat_group)
       expect(response).to render_template :index
     end
