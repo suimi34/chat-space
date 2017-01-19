51314f7046620bce71ed2a1d1b96aa905025e839
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-19 17:39:08 +0900

    delete unnecessary routes

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -13,40 +13,39 @@ describe MessagesController, type: :controller do
     it "populates an array of messages" do
       chat_group
       messages = create_list(:message, 5, chat_group_id: chat_group.id)
-      get :index, params: { chat_group_id: chat_group }, use_routes: chat_group_messages_path(chat_group)
+      get :index, params: { chat_group_id: chat_group }
       expect(assigns(:messages)).to match_array(messages)
     end
 
     it "renders the :index template" do
       chat_group
-      get :index, params: { chat_group_id: chat_group }, use_routes: chat_group_messages_path(chat_group)
+      get :index, params: { chat_group_id: chat_group }
       expect(response).to render_template :index
     end
 
     it "populates a variable of message" do
       chat_group
       message = create(:message)
-      get :index, params: { chat_group_id: chat_group }, use_routes: chat_group_messages_path(chat_group)
+      get :index, params: { chat_group_id: chat_group }
       expect(assigns(:message)).to be_a_new(Message)
     end
 
     it "populates a variable of chat_group" do
       chat_group
-      get :index, params: { chat_group_id: chat_group }, use_routes: chat_group_messages_path(chat_group)
+      get :index, params: { chat_group_id: chat_group }
       expect(assigns(:chat_group)).to eq chat_group
     end
 
     it "populates an array of users" do
       chat_group
       users = chat_group.users
-      get :index, params: { chat_group_id: chat_group }, use_routes: chat_group_messages_path(chat_group)
+      get :index, params: { chat_group_id: chat_group }
       expect(assigns(:users)).to match_array(users)
     end
 
     it "populates an array of chat_groups" do
-      chat_group
       chat_groups = user.chat_groups
-      get :index, params: { chat_group_id: chat_group }, use_routes: chat_group_messages_path(chat_group)
+      get :index, params: { chat_group_id: chat_group }
       expect(assigns(:chat_groups)).to match_array(chat_groups)
     end
   end
