24146af9157daca2ad88c45b7098c5879b6ddba9
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-20 17:48:39 +0900

    refactoring chat_group

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 describe MessagesController, type: :controller do
   let(:user) { create(:user) }
   let(:chat_group) { create(:chat_group) }
+  let(:chat_group) do
+    chat_group = user.chat_groups[0]
+  end
 
   before do
     sign_in user
@@ -11,40 +14,34 @@ describe MessagesController, type: :controller do
   describe "GET #index" do
 
     it "renders the :index template" do
-      chat_group = user.chat_groups[0]
       get :index, params: { chat_group_id: chat_group }
       expect(response).to render_template :index
     end
 
     it "populates an array of messages" do
-      chat_group = user.chat_groups[0]
       messages = create_list(:message, 5, chat_group_id: chat_group.id)
       get :index, params: { chat_group_id: chat_group }
       expect(assigns(:messages)).to match_array(messages)
     end
 
     it "populates a variable of message" do
-      chat_group = user.chat_groups[0]
       message = build(:message)
       get :index, params: { chat_group_id: chat_group }
       expect(assigns(:message)).to be_a_new(Message)
     end
 
     it "populates a variable of chat_group" do
-      chat_group = user.chat_groups[0]
       get :index, params: { chat_group_id: chat_group }
       expect(assigns(:chat_group)).to eq chat_group
     end
 
     it "populates an array of users" do
-      chat_group = user.chat_groups[0]
       users = chat_group.users
       get :index, params: { chat_group_id: chat_group }
       expect(assigns(:users)).to match_array(users)
     end
 
     it "populates an array of chat_groups" do
-      chat_group = user.chat_groups[0]
       chat_groups = user.chat_groups
       get :index, params: { chat_group_id: chat_group }
       expect(assigns(:chat_groups)).to match_array(chat_groups)
@@ -54,21 +51,18 @@ describe MessagesController, type: :controller do
   describe "POST #create" do
 
     it "creates new message in database" do
-      chat_group = user.chat_groups[0]
       message = build(:message, chat_group_id: chat_group.id, user_id: user.id)
       post :create,  params: {chat_group_id: chat_group.id, message: { body: message.body } }
       change(Message, :count).by(1)
     end
 
     it "redirects to chat_group_messages_path" do
-      chat_group = user.chat_groups[0]
       message = build(:message, chat_group_id: chat_group.id, user_id: user.id)
       post :create,  params: {chat_group_id: chat_group.id, message: { body: message.body } }
       expect(response).to redirect_to chat_group_messages_path(chat_group)
     end
 
     it "fails to save if input is invalid" do
-      chat_group = user.chat_groups[0]
       message = build(:message, :as_invalid_input)
       post :create,  params: {chat_group_id: chat_group.id, message: { body: message.body } }
       expect(flash[:alert]).to match("メッセージが入力されていません。")
