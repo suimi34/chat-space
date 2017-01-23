a7da33373a71f19cdc747c3e082015bbc48e32dc
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-23 15:42:43 +0900

    modify js file to add new message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,12 +1,15 @@
 $(function() {
   function buildHtmlBody(message) {
-    var html = $('.view__messages__message').append(message);
+    var html = $('<div class="view__messages__message__body">').append(message);
     return html;
   }
 
   $('.view__message--new').on('submit', function(){
     var messageInput = $('.view__message--new__input');
     var message = messageInput.val();
+    var html = buildHtmlBody(message);
+    var element = $("<div>", { class: "view__messages__message"}).append(html);
+    $('.view__messages').append(element);
 
     $.ajax({
       type: 'POST',
@@ -19,8 +22,6 @@ $(function() {
       dataType: 'json'
     })
     .done(function(data) {
-      var html = buildHtmlBody(message);
-      $('.view__messages').push(html);
       messageInput.val('');
     })
     .fail(function() {
