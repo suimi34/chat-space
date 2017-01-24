6bc8458eb2ec0307c37a8d8494c1031e4557e334
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-24 21:26:13 +0900

    modify controller file

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,8 +12,8 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     if @message.save
       respond_to do |format|
-        format.html { redirect_to redirect_to chat_group_messages_path(@chat_group) }
-        format.json { render 'create', handlers: 'jbuilder' }
+        format.html { redirect_to chat_group_messages_path(@chat_group) }
+        format.json
       end
     else
       flash[:alert] = "メッセージが入力されていません。"
