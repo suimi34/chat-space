0bf26b090aed67a27123c07a0a89e3da8f1c80eb
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-28 20:44:27 +0900

    add input element

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,10 +2,16 @@ $(function() {
 
   $("[id=btn]").click(function(e){
     e.preventDefault();
+    var userId = this.getAttribute('data-user-id')
     if ($(this).attr("class") === "chat-group-user__btn--add"){
       $(this).removeClass("chat-group-user__btn--add").addClass("chat-group-user__btn--remove");
       this.textContent = "削除";
       var addUser = $(this).parents('.chat-group-user');
+      var ele = document.createElement('input');
+      ele.type = "hidden";
+      ele.style = "border:none";
+      ele.value = userId;
+      addUser.append(ele);
       $("#added_users").append(addUser);
     } else {
       $(this).removeClass("chat-group-user__btn--remove").addClass("chat-group-user__btn--add");
