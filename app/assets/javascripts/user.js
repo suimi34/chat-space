59daccba5ae6d9466e06aa275e5e64540a334cc3
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-30 19:47:16 +0900

    modify process for changing ban

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,29 +1,39 @@
 $(function() {
 
-  $("[id=btn]").click(function(){
-    if ($(this).attr("class") === "chat-group-user__btn--add"){
-      var addUserId = this.getAttribute('data-user-id')
-      $(this).removeClass("chat-group-user__btn--add").addClass("chat-group-user__btn--remove");
-      this.textContent = "削除";
-      var addUser = $(this).parents('.chat-group-user');
-      var ele = document.createElement('input');
-      ele.id = "hidden_id";
-      ele.type = "hidden";
-      ele.name = "chat_group[user_ids][]";
-      ele.style = "border:none";
-      ele.value = addUserId;
-      $(this).removeAttr('data-user-id');
-      addUser.append(ele);
-      $("#added_users").append(addUser);
-    } else {
-      var removeUser = $(this).parents('.chat-group-user');
-      var removeUserInput = removeUser.children('input');
-      var removeUserId = removeUserInput.val();
-      $(this).removeClass("chat-group-user__btn--remove").addClass("chat-group-user__btn--add");
-      this.textContent = "追加";
-      $(this).attr('data-user-id', removeUserId);
-      removeUserInput.remove();
-      $("#user_info").append(removeUser);
-    }
+  $(".chat-group-user").on('click', 'p', function(){
+    var addUserId = this.getAttribute('data-user-id');
+    var btn = $(this).parent();
+    var addUser = $(this).parents('.chat-group-user');
+    var ele = document.createElement('input');
+    ele.id = "hidden_id";
+    ele.type = "hidden";
+    ele.name = "chat_group[user_ids][]";
+    ele.style = "border:none";
+    ele.value = addUserId;
+    var removeBtn = $("<li>",{
+      "class" : "chat-group-user__btn--remove",
+      text: "削除"
+    });
+    $(this).remove();
+    btn.append(removeBtn);
+    addUser.append(ele, btn);
+    $("#added_users").append(addUser);
+  });
+
+  $(".chat-group-user").on('click', 'li', function(){
+    var removeUser = $(this).parents('.chat-group-user');
+    var btn = $(this).parent();
+    var removeUserInput = removeUser.children('input');
+    var removeUserId = removeUserInput.val();
+    var addBtn = $("<p>",{
+      "class" : "chat-group-user__btn--add",
+      text: "追加"
+    });
+    addBtn.attr('data-user-id', removeUserId),
+    btn.append(addBtn);
+    removeUser.append(btn);
+    $(this).remove();
+    removeUserInput.remove();
+    $('#user_info').append(removeUser);
   });
 });
