828dd44cf3c09a8157de21d64e54462e99e44523
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-31 15:49:06 +0900

    add process to son format

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def user_list
-    results = User.where('name LIKE(?)', '%' + params[:name] + '%')
+    @results = User.where('name LIKE(?)', '%' + params[:name] + '%')
+    respond_to do |format|
+      format.html { redirect_to new_chat_group_path }
+      format.json
+    end
+  end
 end
