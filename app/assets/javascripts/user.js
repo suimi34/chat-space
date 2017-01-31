a43a2b6f520aa9af61b248e567b3259166547ba9
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-01-31 11:31:23 +0900

    refactoring changing ban process

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,39 +1,27 @@
 $(function() {
 
-  $(".chat-group-user").on('click', 'p', function(){
-    var addUserId = this.getAttribute('data-user-id');
-    var btn = $(this).parent();
+  $(".chat-group-user").on("click", ".chat-group-user__btn--add", function(){
+    var addUserId = this.getAttribute("data-user-id");
     var addUser = $(this).parents('.chat-group-user');
-    var ele = document.createElement('input');
+    this.textContent = "削除"
+    $(this).removeClass("chat-group-user__btn--add").addClass("chat-group-user__btn--remove");
+    var ele = document.createElement("input");
     ele.id = "hidden_id";
     ele.type = "hidden";
     ele.name = "chat_group[user_ids][]";
     ele.style = "border:none";
     ele.value = addUserId;
-    var removeBtn = $("<li>",{
-      "class" : "chat-group-user__btn--remove",
-      text: "削除"
-    });
-    $(this).remove();
-    btn.append(removeBtn);
-    addUser.append(ele, btn);
+    addUser.append(ele);
     $("#added_users").append(addUser);
   });
 
-  $(".chat-group-user").on('click', 'li', function(){
-    var removeUser = $(this).parents('.chat-group-user');
-    var btn = $(this).parent();
+  $(".chat-group-user").on("click", ".chat-group-user__btn--remove", function(){
+    var removeUser = $(this).parents(".chat-group-user");
     var removeUserInput = removeUser.children('input');
     var removeUserId = removeUserInput.val();
-    var addBtn = $("<p>",{
-      "class" : "chat-group-user__btn--add",
-      text: "追加"
-    });
-    addBtn.attr('data-user-id', removeUserId),
-    btn.append(addBtn);
-    removeUser.append(btn);
-    $(this).remove();
+    this.textContent = "追加"
+    $(this).removeClass("chat-group-user__btn--remove").addClass("chat-group-user__btn--add");
     removeUserInput.remove();
-    $('#user_info').append(removeUser);
+    $("#user_info").append(removeUser);
   });
 });
