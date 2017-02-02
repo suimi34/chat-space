2c2b26c899f2064760584ddce29c1459b0878dcb
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-02-02 20:06:56 +0900

    modify AWS region name

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     provider:               'AWS',
     aws_access_key_id:      ENV['S3_ACCESS_KEY_ID'],
     aws_secret_access_key:  ENV['S3_SECRET_ACCESS_KEY'],
-    region:                 ENV['AWS_REGION']
+    region:                 ENV['S3_AWS_REGION']
   }
 
   case Rails.env
