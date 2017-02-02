1eb0721f70a021d18f4a5bf313c713e14c14a1ec
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-02-02 18:01:02 +0900

    modify storage degignation

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider:               ENV['AWS'],
+    provider:               'AWS',
     aws_access_key_id:      ENV['S3_ACCESS_KEY_ID'],
     aws_secret_access_key:  ENV['S3_SECRET_ACCESS_KEY'],
     region:                 ENV['AWS_REGION']
@@ -10,5 +10,8 @@ CarrierWave.configure do |config|
     when 'production', 'development'
       config.fog_directory = ENV['S3_BUCKET_NAME']
       config.asset_host = ENV['S3_ASSET_HOST']
+
+    when 'test'
+      :file
   end
 end
