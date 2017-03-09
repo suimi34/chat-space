83abf41224b6e97218eedd92e5a5d1a63f834d3b
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-03-09 19:32:52 +0900

    modify deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.7.2"
 
 set :application, "chat-space"
-set :repo_url, "git@github.com:chat-space.git"
+set :repo_url, "git@github-frongrasanf:frongrasanf/chat-space.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -10,10 +10,12 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/tc-ex-chat-space.pem']
+                  keys: ['~/.ssh/tc-ex-first.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+
+set :branch, ENV['BRANCH'] || "master"
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
