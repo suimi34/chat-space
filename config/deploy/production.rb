fd1adfe99c9f718c53cf5847213f3a859ee507bf
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-03-09 19:10:05 +0900

    modify deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server '13.112.100.177', user: 'ec2-user', roles: %w{app db web}
 
 # role-based syntax
 # ==================
