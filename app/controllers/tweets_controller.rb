e32865cc45c36a6eaa2a1df6a4d4e5a19138b071
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-03-14 12:47:36 +0900

    modify tweet controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,22 +1,23 @@
 class TweetsController < ApplicationController
   def search
-  client = Twitter::REST::Client.new do |config|
-    config.consumer_key         = ENV['TWITTER_CONSUMER_KEY']
-    config.consumer_secret      = ENV['TWITTER_CONSUMER_SECRET']
-  end
-
-  @tweets = []
-  since_id = nil
-
-  if params[:keyword].present?
-
-    tweets = client.search(params[:keyword], count: 10, result_type: "recent", exclude: "retweets", since_id: since_id)
-    tweets.take(10).each do |tw|
-      tweet = Tweet.new(tw.full_text)
-      @tweets << tweet
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key         = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret      = ENV['TWITTER_CONSUMER_SECRET']
     end
-  end
-  respond_to do |format|
-    format.html
+
+    @tweets = []
+    since_id = nil
+
+    if params[:keyword].present?
+
+      tweets = client.search(params[:keyword], count: 10, result_type: "recent", exclude: "retweets", since_id: since_id)
+      tweets.take(10).each do |tw|
+        tweet = Tweet.new(tw.full_text)
+        @tweets << tweet
+      end
+    end
+    respond_to do |format|
+      format.html
+    end
   end
 end
