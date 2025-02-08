95d8e36147e9b47fe600f79f3c2aa467b776d4f8
Susumu Ishikawa <susumu2934@gmail.com>
2025-02-08 17:51:51 +0900

    update carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,16 @@
 CarrierWave.configure do |config|
+  config.provider = 'fog/google',
   config.fog_credentials = {
-    provider:               'AWS',
-    aws_access_key_id:      ENV['S3_ACCESS_KEY_ID'] || '',
-    aws_secret_access_key:  ENV['S3_SECRET_ACCESS_KEY'] || '',
-    region:                 ENV['S3_AWS_REGION'] || ''
+    provider: 'Google',
+    google_project: ENV['GOOGLE_PROJECT_ID'],
+    google_application_default: true,
   }
+  config.fog_directory = ENV['GOOGLE_CLOUD_BUCKET_NAME'] || ''
 
   case Rails.env
     when 'production', 'development'
-      config.fog_directory = ENV['S3_BUCKET_NAME'] || ''
-      config.asset_host = ENV['S3_ASSET_HOST'] || ''
+      config.fog_directory = ENV['GOOGLE_CLOUD_BUCKET_NAME'] || ''
+      # config.asset_host = ENV['S3_ASSET_HOST'] || ''
 
     when 'test'
       :file
