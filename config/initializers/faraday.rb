5bf36a823fbad4061e650f496f3217c763c75f13
Susumu Arimoto <susumu2934@gmail.com>
2026-05-09 18:23:25 +0900

    fix faraday

--- config/initializers/faraday.rb
+++ config/initializers/faraday.rb
@@ -1,2 +1,5 @@
-# Faraday 2+ has no default HTTP adapter; google-cloud-* relies on Faraday.
+# Faraday 2+ has no default HTTP adapter; google-cloud-* expects one to be set.
+require "faraday"
 require "faraday/net_http"
+
+Faraday.default_adapter = :net_http
